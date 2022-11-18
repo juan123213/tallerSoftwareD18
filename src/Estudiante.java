2c1438a5c6c0cba792222e85d11f33ab96edcb12
Juan <juan.andresgiraldo@hotmail.com>
2022-11-18 15:35:24 -0500

    creamos las clases que vamos a utilizar

---
+++ src/Estudiante.java
@@ -0,0 +1,18 @@
+public class Estudiante {
+    private String nombre;
+    private String cedula;
+    private String codigosMaterias;
+
+    public Estudiante (String cedula, String nombre){
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public boolean AdicionarMateria(String codigosMaterias){
+    return true;
+    }
+
+    public String toString(){
+        return "´";
+    }
+}
---
+++ src/VentaInscripciones.java
@@ -0,0 +1,6 @@
+public class VentaInscripciones {
+
+    public static void main(String[] args){
+
+    }
+}
